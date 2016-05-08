6324f32e0b5ecc87f68b5b20001cc279f6629d34
Taurit <git@taurit.pl>
2016-05-08 16:46:32 +0200

    Extended list of supported platforms with .NET 3.5, and .NET 4.0

---
+++ Taurit.NameHelper.Polish.Tests.Compatibility.DotNet461/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Taurit.NameHelper.Polish.Tests.Compatibility.DotNet461
+{
+    /// <summary>
+    ///     Simple program targeting .NET 4.6.1 to test library combatilbility with .NET projects
+    /// </summary>
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var testedName = "Adam";
+            IPolishNameFlectionHelper helper = new PolishNameFlectionHelper();
+            string modifiedName = helper.GetFirstNameInGenitiveForm(testedName);
+
+            Debug.Assert(modifiedName == "Adama");
+            Console.WriteLine($"Name '{testedName}' was successfuly converted to '{modifiedName}' by the library.");
+            Console.ReadKey();
+        }
+    }
+}
