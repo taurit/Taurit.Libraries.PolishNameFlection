2c623946a25f0ea6965ecce3691e15bc7be692d1
Taurit <git@taurit.pl>
2016-05-21 19:01:35 +0200

    Added test project to verify library compatibility with .NET Core RC2 projects

---
+++ Taurit.NameHelper.Polish.Tests.Compatibility.DotNetCoreRC2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Taurit.NameHelper.Polish.Tests.Compatibility.DotNetCoreRC2
+{
+    public class Program
+    {
+        /// <summary>
+        ///     Simple program targeting .NET Core RC2 to test library combatilbility with .NET projects
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            var testedName = "Adam";
+            IPolishNameFlectionHelper helper = new PolishNameFlectionHelper();
+            string modifiedName = helper.GetFirstName(testedName, Case.Genitive);
+
+            Debug.Assert(modifiedName == "Adama");
+            Console.WriteLine($"Name '{testedName}' was successfuly converted to '{modifiedName}' by the library.");
+            Console.ReadKey();
+        }
+    }
+}
